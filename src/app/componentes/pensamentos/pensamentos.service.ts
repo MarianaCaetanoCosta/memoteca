e25cbe2c5da53eb5dbe0a76ef3c139470038369d
Mariana Caetano <mariana.cae.costa@gmail.com>
2023-08-31 00:37:22 -0300

    #Injeção de dependências - HttpClient

--- src/app/componentes/pensamentos/pensamentos.service.ts
+++ src/app/componentes/pensamentos/pensamentos.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentosService {
 
-  constructor() { }
+  // dependencia da classe HttpClient
+  constructor(private http: HttpClient) { }
 }
